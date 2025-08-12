42b00e1bff3c9234ddbb0bc9085b2d2db637a840
Pavan Sai Venkata Durga Prashanth Korapati <134833398+kpsvdp@users.noreply.github.com>
2025-08-12 01:34:55 +0100

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-// IMPORTANT: Base path set to match GitHub repo name MasalaMeal
-export default defineConfig({
-  plugins: [react()],
-  base: '/MasalaMeal/'
-})
