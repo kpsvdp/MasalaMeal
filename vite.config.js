95e10332b8937ef24cab54f10d4a0f3053e33baa
Pavan Sai Venkata Durga Prashanth Korapati <134833398+kpsvdp@users.noreply.github.com>
2025-08-12 02:11:18 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  base: '/MasalaMeal/'
+  base: '/MasalaMeal/'  // <-- MUST match repo name exactly, case-sensitive
 })
